ee3c2a06bc6b86f83cbe59a832abe7828dbb473f
周忠毅 <rilakcrc35@gmail.com>
2017-08-12 19:54:33 +0800

    pass the home_page_basic_info test

--- website/main/views.py
+++ website/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+import datetime
+
+def get_this_week_range_str():
+    today = datetime.date.today()
+    weekday = (today.weekday()+1)%7
+    sun = today + datetime.timedelta(days = -weekday)
+    sat = today + datetime.timedelta(days = 6-weekday)
+    return f"{sun.month}/{sun.day}~{sat.month}/{sat.day}"
+
+def home_page(request):
+    return render(request, 'home_page.html', {
+            "big_title" : get_this_week_range_str(),
+        })
