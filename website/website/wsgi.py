08f4469cb667394771568402b92ef9415d9f6acf
周忠毅 <rilakcrc35@gmail.com>
2017-08-18 00:57:00 +0800

    Deploy on heroku

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
+# website/website/wsgi.py
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
